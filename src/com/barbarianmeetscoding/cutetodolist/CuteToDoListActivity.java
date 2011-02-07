d3626d29bf19c2dc52b07fc20d84a3a3782eaaed
vintharas <jaime.gg83@gmail.com>
2011-02-07 19:47:51 +0100

    fixed importing problems

--- src/com/barbarianmeetscoding/cutetodolist/CuteToDoListActivity.java
+++ src/com/barbarianmeetscoding/cutetodolist/CuteToDoListActivity.java
@@ -13,7 +13,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 public class CuteToDoListActivity extends ListActivity {
 	
